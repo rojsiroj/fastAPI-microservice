f500a9eca82538e94e0dcbca7191404db1e22ae3
rojsiroj <sirojudin.dev@gmail.com>
2022-04-04 17:59:39 +0700

    first commit

---
+++ payment/main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
+import time
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+redis = get_redis_connection(
+    host="redis-17834.c292.ap-southeast-1-1.ec2.cloud.redislabs.com",
+    port=17834,
+    password="ia8L3c9t6N05ypVmapNR4WrauSSzv0oW",
+    decode_responses=True
+)
+
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str  # pending, completed, refunded
+
+    class Meta:
+        database = redis
+
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    order = Order.get(pk)
+    redis.xadd('refund_order', order.dict(), '*')
+    return order
+
+
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks):
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' %
+                       body['product_id'])
+    product = req.json()
+
+    order = Order(product_id=body['product_id'], price=product['price'],
+                  fee=0.2 * product['price'], total=1.2 * product['price'], quantity=body['quantity'], status='pending')
+    order.save()
+    background_tasks.add_task(order_completed, order)
+
+    return order
+
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    redis.xadd('order_completed', order.dict(), '*')
